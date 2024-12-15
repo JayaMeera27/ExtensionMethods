92fb26fe1e48a40fc0850714b01d10a6a49f2ff9
Jaya Meera B <jayameera272@outlook.com>
2024-12-15 11:57:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ExtensionMethod_Class;
+using Person;
+
+
+class Program
+{
+    public static void Main()
+    {
+        Person1 p = new Person1 { name = "Meera", dateOfBirth = new DateTime(1999,2,27) };
+        string displayText = $"Age of {p.name} is {p.CalculateAge()}";
+        Console.WriteLine(displayText );
+        Console.WriteLine($"Word Count is {displayText.WordCount()}");
+
+
+    }
+}
